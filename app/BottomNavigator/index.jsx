06dae1bf4d6d346074f1cb368d4ae6bc09380cb9
Luis Adames <adames1601@gmail.com>
2024-08-25 15:42:35 -0400

    menu en desarrollo y form publicacion casi terminado

---
+++ app/BottomNavigator/index.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+import { Animated, StyleSheet, TouchableOpacity, View } from "react-native";
+import { CurvedBottomBarExpo } from "react-native-curved-bottom-bar";
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import Octicons from 'react-native-vector-icons/Octicons';
+import { useNavigation } from "@react-navigation/native";
+import HomeScreen from "../home";
+import SearchScreen from "../login";
+import MapScreen from "../sign_up";
+import ProfileScreen from "../FormPublicacion";
+import { BlurView } from '@react-native-community/blur'; // Importa BlurView
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const BaseNavigator = () => {
+  const navigation = useNavigation();
+  const _renderIcon = (routeName, selectedTab) => {
+    let IconComponent = Feather;
+    let iconName = ""
+
+    switch (routeName) {
+        case "Home":
+            IconComponent = AntDesign;
+            iconName = "home";
+            break;
+        case "Search":
+            IconComponent = AntDesign;
+            iconName = "search1";
+            break;
+        case "Map":
+            IconComponent = Feather;
+            iconName = "book-open";
+            break;
+        case "profile":
+            IconComponent = Octicons;
+            iconName = "person";
+            break;
+        default:
+            break;
+    }
+
+    return (
+        <IconComponent
+            name={iconName}
+            size={32}
+            color={routeName === selectedTab ? "#3E86B9" : "#556871"}
+        />
+    );
+  };
+  const renderTabBar = ({ routeName, selectedTab, navigate }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => navigate(routeName)}
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {_renderIcon(routeName, selectedTab)}
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#F3F7FD" }}>
+    <View style={{ flex: 1}}>
+      <CurvedBottomBarExpo.Navigator
+        style={styles.bottomBar}
+        height={60}
+        circleWidth={55}
+        bgColor="#F3F7FD"
+        initialRouteName="Home"
+        borderTopLeftRight={true}
+        renderCircle={({ selectedTab, navigate }) => (
+          <Animated.View>
+            <TouchableOpacity
+              style={styles.btnCircle}
+              onPress={() => navigation.navigate("PostScreen")}
+            >
+              <AntDesign name="plus" size={32} color="#F3F7FD" />
+            </TouchableOpacity>
+          </Animated.View>
+        )}
+        tabBar={renderTabBar}
+      >
+        <CurvedBottomBarExpo.Screen
+          name="Home"
+          position="LEFT"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+          
+        />
+        <CurvedBottomBarExpo.Screen
+          name="Search"
+          position="LEFT"
+          options={{
+            headerShown: false,
+          }}
+          component={SearchScreen}
+        />
+        <CurvedBottomBarExpo.Screen
+          name="Map"
+          position="RIGHT"
+          options={{
+            headerShown: false,
+          }}
+          component={MapScreen}
+        />
+        <CurvedBottomBarExpo.Screen
+          name="profile"
+          component={ProfileScreen}
+          position="RIGHT"
+          options={{ headerShown: false }}
+        />
+      </CurvedBottomBarExpo.Navigator>
+    </View>
+    </SafeAreaView>
+  );
+};
+export default BaseNavigator;
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  button: {
+    marginVertical: 5,
+  },
+  bottomBar: {
+    position: "absolute",
+    borderRadius: 20,
+    elevation: 15,
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: -10,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 10,
+  },
+  btnCircle: {
+    width: 60,
+    height: 60,
+    borderRadius: 35,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#00D0A1",
+    padding: 10,
+    shadowColor: "#1A1A23",
+    shadowOffset: {
+      width: 0,
+      height: 0.5,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 20,
+    bottom: 30,
+  },
+  imgCircle: {
+    width: 30,
+    height: 30,
+    tintColor: "gray",
+  },
+  img: {
+    width: 30,
+    height: 30,
+  },
+});
--- app/FormPublicacion/index.jsx
+++ app/FormPublicacion/index.jsx
@@ -1,229 +1,266 @@
 import { StyleSheet, Text, View,Image, TouchableOpacity,ScrollView,StatusBar } from "react-native";
-import { Link, } from 'expo-router';
+import { Link, useRouter} from 'expo-router';
 import { Button,Icon } from "react-native-paper";
 import { useState,useEffect } from "react";
 import InputSignUp from "../../components/input_sign_up";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-
+import InputFecha from "../../components/input_fecha";
+import DropdownComponent from "../../components/dropdown";
+import BottomNavigator from "../BottomNavigator";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function Page() {
 
-    const validationSchema = Yup.object({
-        nombre: Yup.string().required("Este campo es obligatorio"),
-        fecha_nacimiento: Yup.date().required("Este campo es obligatorio"),
-        tipo_documento: Yup.string().required("Este campo es obligatorio"),
-        documento: Yup.string().required("Este campo es obligatorio"),
-        telefono: Yup.string().required("Este campo es obligatorio"),
-        fecha_desaparicion: Yup.date().required("Este campo es obligatorio"),
-        relacion_desaparecido: Yup.string().required("Este campo es obligatorio"),
-        contacto: Yup.string().required("Este campo es obligatorio"),
-        ubicacion: Yup.string().required("Este campo es obligatorio"),
-        descripcion_desaparecido: Yup.string().required("Este campo es obligatorio"),
-    })
+  const [showDateModal, setShowDateModal] = useState(false);
+  const router = useRouter();
 
-    const [pressed, setPressed] = useState({
-        nombre: false,
-        fecha_nacimiento: false,
-        tipo_documento: false,
-        documento: false,
-        telefono: false,
-        fecha_desaparicion: false,
-        relacion_desaparecido: false,
-        contacto: false,
-        ubicacion: false,
-        descripcion_desaparecido: false,
-    });
+  // Estado para el valor seleccionado del dropdown
+  const [selectedValue, setSelectedValue] = useState('');
 
-    const formik = useFormik({
-        initialValues: {
-            nombre: "",
-            fecha_nacimiento: "",
-            tipo_documento: "",
-            documento: "",
-            telefono: "",
-            fecha_desaparicion: "",
-            relacion_desaparecido: "",
-            contacto: "",
-            ubicacion: "",
-            descripcion_desaparecido: "",
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            console.log(values);
-        },
-    });
+  const [error, setError] = useState(false);
 
-    const handlePress = (field) => {
-        setPressed({ ...pressed, [field]: true });
-    };
+  // Manejo del evento de selección
+  const handleSelect = (value) => {
+    setSelectedValue(value);
+    setPressed(true); // Puedes ajustar según la lógica de tu aplicación
+  };
+  
+  const items = [
+    { value: 'Cédula', key: '1' },
+    { value: 'Pasaporte', key: '2' }
+  ];
+  const validationSchema = Yup.object({
+    nombre: Yup.string().required("Este campo es obligatorio"),
+    tipo_documento: Yup.string().required("Este campo es obligatorio"),
+    documento: Yup.string().required("Este campo es obligatorio"),
+    telefono: Yup.string().required("Este campo es obligatorio"),
+    relacion_desaparecido: Yup.string().required("Este campo es obligatorio"),
+    contacto: Yup.string().required("Este campo es obligatorio"),
+    ubicacion: Yup.string().required("Este campo es obligatorio"),
+    descripcion_desaparecido: Yup.string().required(
+      "Este campo es obligatorio"
+    ),
+  });
 
-    return (
-      <View className="flex-1  bg-[#F3F7FD]">
-        <StatusBar
-          hidden={false}
-          backgroundColor={"#F3F7FD"}
-          barStyle={"dark-content"}
-        />
-        {/* Boton back */}
+  const [pressed, setPressed] = useState({
+    nombre: false,
+    fecha_nacimiento: false,
+    tipo_documento: false,
+    documento: false,
+    telefono: false,
+    fecha_desaparicion: false,
+    relacion_desaparecido: false,
+    contacto: false,
+    ubicacion: false,
+    descripcion_desaparecido: false,
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      nombre: "",
+      fecha_nacimiento: new Date(),
+      tipo_documento: "",
+      documento: "",
+      telefono: "",
+      fecha_desaparicion: new Date(),
+      relacion_desaparecido: "",
+      contacto: "",
+      ubicacion: "",
+      descripcion_desaparecido: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const handlePress = (field) => {
+    setPressed({ ...pressed, [field]: true });
+  };
+
+  return (
+    <View className="flex-1  bg-[#F3F7FD]">
+      
+      <StatusBar
+        hidden={false}
+        backgroundColor={"#F3F7FD"}
+        barStyle={"dark-content"}
+      />
+      {/* Boton back */}
+      <View className="flex">
+        <View className="flex mx-[4.5vw] my-[1vh]">
+          <TouchableOpacity onPress={() => router.push("../home")}>
+            <Image
+              source={require("../../assets/sign_up/flecha-izquierda.png")}
+              className="w-[10vw] h-[calc(4.5vh)]"
+            ></Image>
+          </TouchableOpacity>
+        </View>
+      </View>
+      
+      {/* Formulario */}
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContent}
+        className=" flex  mb-[calc(1.5vh)]"
+      >
         <View className="flex">
-          <View className="flex mx-[4.5vw] my-[1vh]">
-            <TouchableOpacity onPress={() => router.push("../home")}>
-              <Image
-                source={require("../../assets/sign_up/flecha-izquierda.png")}
-                className="w-[10vw] h-[calc(4.5vh)]"
-              ></Image>
+          <Text className="text-[24px] text-[#233E58] font-extrabold py-[1vh] mx-[-42.5vw]">
+            Crear publicación de{"\n"}
+            Persona Desaparecida
+          </Text>
+        </View>
+        <View className=" flex w-[calc(85.380vw)]">
+          {/* Input Nombre */}
+          <InputSignUp
+            separation={0.028}
+            label="Nombre"
+            placeholder="Ingrese el nombre"
+            value={formik.values.nombre}
+            onChangeText={formik.handleChange("nombre")}
+            onBlur={() => handlePress("nombre")}
+            error={pressed.nombre && formik.errors.nombre}
+          />
+
+          {/* Input Fecha de Nacimiento */}
+          <InputFecha
+            label={"Fecha de Nacimiento"}
+            separation={0.028}
+            value={formik.values.fecha_nacimiento}
+            placeholder={"Seleccione su fecha de Nacimiento"}
+            id_name={"fecha_nacimiento"}
+            setFieldValue={formik.setFieldValue}
+            fiedName={"fecha_nacimiento"}
+            pressed={pressed.fechaNacimiento}
+            handlePressed={() =>
+              setPressed({ ...pressed, fechaNacimiento: true })
+            }
+            error={formik.errors.fecha_nacimiento}
+            showDateModal={showDateModal}
+            setShowDateModal={setShowDateModal}
+          />
+
+          <DropdownComponent
+            label="Tipo de documento"
+            placeholder="Seleccione el tipo de documento"
+            data={items}
+            value={selectedValue}
+            setSelectedValue={handleSelect}
+            separation={0.028}
+            pressed={pressed.tipo_documento}
+            error={formik.errors.tipo_documento}
+          
+          />
+
+          {/* Input Documento */}
+          <InputSignUp
+            separation={0.028}
+            label="Documento"
+            placeholder="Documento"
+            value={formik.values.documento}
+            onChangeText={formik.handleChange("documento")}
+            onBlur={() => handlePress("documento")}
+            error={pressed.documento && formik.errors.documento}
+          />
+
+          {/* Input Teléfono */}
+          <InputSignUp
+            separation={0.028}
+            label="Teléfono contacto"
+            placeholder="809-000-0000"
+            value={formik.values.telefono}
+            onChangeText={formik.handleChange("telefono")}
+            onBlur={() => handlePress("telefono")}
+            error={pressed.telefono && formik.errors.telefono}
+          />
+
+          {/* Input Fecha de desaparición */}
+          <InputFecha
+            label={"Fecha de desaparición"}
+            separation={0.028}
+            value={formik.values.fecha_desaparicion}
+            placeholder={"Seleccione la fecha de desaparición"}
+            id_name={"fecha_desaparicion"}
+            setFieldValue={formik.setFieldValue}
+            fiedName={"fecha_desaparicion"}
+            pressed={pressed.fecha_desaparicion}
+            handlePressed={() =>
+              setPressed({ ...pressed, fecha_desaparicion: true })
+            }
+            error={formik.errors.fecha_desaparicion}
+            showDateModal={showDateModal}
+            setShowDateModal={setShowDateModal}
+          />
+
+          {/* Input Relación con el desaparecido */}
+          <InputSignUp
+            separation={0.028}
+            label="Relación con el desaparecido"
+            placeholder="ej. Madre, Padre, Hermano"
+            value={formik.values.relacion_desaparecido}
+            onChangeText={formik.handleChange("relacion_desaparecido")}
+            onBlur={() => handlePress("relacion_desaparecido")}
+            error={
+              pressed.relacion_desaparecido &&
+              formik.errors.relacion_desaparecido
+            }
+          />
+
+          {/* Input Contacto */}
+          <InputSignUp
+            separation={0.028}
+            label="Contacto"
+            placeholder="Otra información de contacto"
+            value={formik.values.contacto}
+            onChangeText={formik.handleChange("contacto")}
+            onBlur={() => handlePress("contacto")}
+            error={pressed.contacto && formik.errors.contacto}
+          />
+
+          {/* Input Ubicación */}
+          <InputSignUp
+            separation={0.028}
+            label="Ubicación"
+            placeholder="Ubicación"
+            value={formik.values.ubicacion}
+            onChangeText={formik.handleChange("ubicacion")}
+            onBlur={() => handlePress("ubicacion")}
+            error={pressed.ubicacion && formik.errors.ubicacion}
+          />
+
+          {/* Input Descripción del desaparecido */}
+          <InputSignUp
+            separation={0.028}
+            label="Descripción del desaparecido"
+            placeholder="ej. Estatura, color de piel, color de ojos"
+            value={formik.values.descripcion_desaparecido}
+            onChangeText={formik.handleChange("descripcion_desaparecido")}
+            onBlur={() => handlePress("descripcion_desaparecido")}
+            error={
+              pressed.descripcion_desaparecido &&
+              formik.errors.descripcion_desaparecido
+            }
+          />
+
+          {/* Subit foto */}
+
+          <View className="flex flex-col w-full">
+            <TouchableOpacity
+              activeOpacity={0.7}
+              className="bg-[#3E86B9] flex mx-auto w-[85vw] h-[7vh] rounded-md justify-center  align-middle"
+              onPress={formik.handleSubmit}
+            >
+              <Text className="flex w-full text-center text-[16px] font-bold text-[#F3F7FD]">
+                Publicar
+              </Text>
             </TouchableOpacity>
           </View>
+          
         </View>
-
-        {/* Formulario */}
-        <ScrollView
-          contentContainerStyle={styles.scrollViewContent}
-          className=" flex  mb-[calc(1.5vh)]"
-        >
-          <View className="flex">
-            <Text className="text-[24px] text-[#233E58] font-extrabold py-[1vh] mx-[-42.5vw]">
-              Crear publicación de{"\n"}
-              Persona Desaparecida
-            </Text>
-          </View>
-          <View className=" flex w-[calc(85.380vw)]">
-            {/* Input Nombre */}
-            <InputSignUp
-              separation={0.028}
-              label="Nombre"
-              placeholder="Ingrese el nombre"
-              value={formik.values.nombre}
-              onChangeText={formik.handleChange("nombre")}
-              onBlur={() => handlePress("nombre")}
-              error={pressed.nombre && formik.errors.nombre}
-            />
-
-            {/* Input Fecha de nacimiento */}
-            <InputSignUp
-              separation={0.028}
-              label="Fecha de nacimiento"
-              placeholder="dd/mm/aaaa"
-              value={formik.values.fecha_nacimiento}
-              onChangeText={formik.handleChange("fecha_nacimiento")}
-              onBlur={() => handlePress("fecha_nacimiento")}
-              error={pressed.fecha_nacimiento && formik.errors.fecha_nacimiento}
-            />
-
-            {/* Input Tipo de documento */}
-            <InputSignUp
-              separation={0.028}
-              label="Tipo de documento"
-              placeholder="Tipo de documento"
-              value={formik.values.tipo_documento}
-              onChangeText={formik.handleChange("tipo_documento")}
-              onBlur={() => handlePress("tipo_documento")}
-              error={pressed.tipo_documento && formik.errors.tipo_documento}
-            />
-
-            {/* Input Documento */}
-            <InputSignUp
-              separation={0.028}
-              label="Documento"
-              placeholder="Documento"
-              value={formik.values.documento}
-              onChangeText={formik.handleChange("documento")}
-              onBlur={() => handlePress("documento")}
-              error={pressed.documento && formik.errors.documento}
-            />
-
-            {/* Input Teléfono */}
-            <InputSignUp
-              separation={0.028}
-              label="Teléfono contacto"
-              placeholder="809-000-0000"
-              value={formik.values.telefono}
-              onChangeText={formik.handleChange("telefono")}
-              onBlur={() => handlePress("telefono")}
-              error={pressed.telefono && formik.errors.telefono}
-            />
-
-            {/* Input Fecha de desaparición */}
-            <InputSignUp
-              separation={0.028}
-              label="Fecha de desaparición"
-              placeholder="dd/mm/aaaa"
-              value={formik.values.fecha_desaparicion}
-              onChangeText={formik.handleChange("fecha_desaparicion")}
-              onBlur={() => handlePress("fecha_desaparicion")}
-              error={
-                pressed.fecha_desaparicion && formik.errors.fecha_desaparicion
-              }
-            />
-
-            {/* Input Relación con el desaparecido */}
-            <InputSignUp
-              separation={0.028}
-              label="Relación con el desaparecido"
-              placeholder="ej. Madre, Padre, Hermano"
-              value={formik.values.relacion_desaparecido}
-              onChangeText={formik.handleChange("relacion_desaparecido")}
-              onBlur={() => handlePress("relacion_desaparecido")}
-              error={
-                pressed.relacion_desaparecido &&
-                formik.errors.relacion_desaparecido
-              }
-            />
-
-            {/* Input Contacto */}
-            <InputSignUp
-              separation={0.028}
-              label="Contacto"
-              placeholder="Otra información de contacto"
-              value={formik.values.contacto}
-              onChangeText={formik.handleChange("contacto")}
-              onBlur={() => handlePress("contacto")}
-              error={pressed.contacto && formik.errors.contacto}
-            />
-
-            {/* Input Ubicación */}
-            <InputSignUp
-              separation={0.028}
-              label="Ubicación"
-              placeholder="Ubicación"
-              value={formik.values.ubicacion}
-              onChangeText={formik.handleChange("ubicacion")}
-              onBlur={() => handlePress("ubicacion")}
-              error={pressed.ubicacion && formik.errors.ubicacion}
-            />
-
-            {/* Input Descripción del desaparecido */}
-            <InputSignUp
-              separation={0.028}
-              label="Descripción del desaparecido"
-              placeholder="ej. Estatura, color de piel, color de ojos"
-              value={formik.values.descripcion_desaparecido}
-              onChangeText={formik.handleChange("descripcion_desaparecido")}
-              onBlur={() => handlePress("descripcion_desaparecido")}
-              error={
-                pressed.descripcion_desaparecido &&
-                formik.errors.descripcion_desaparecido
-              }
-            />
-
-            {/* Subit foto */}
-            
-
-            <View className="flex flex-col w-full">
-              <TouchableOpacity
-                activeOpacity={0.7}
-                className="bg-[#3E86B9] flex mx-auto w-[85vw] h-[7vh] rounded-md justify-center  align-middle"
-                onPress={formik.handleSubmit}
-              >
-                <Text className="flex w-full text-center text-[16px] font-bold text-[#F3F7FD]">
-                  Publicar
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </ScrollView>
-      </View>
-    );
+        
+      </ScrollView>
+      <BottomNavigator />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
