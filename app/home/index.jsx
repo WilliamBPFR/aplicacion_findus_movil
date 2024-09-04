eaedf41aa0101c40197dc4203328934ee6baefa5
Luis Adames <adames1601@gmail.com>
2024-09-03 20:54:58 -0400

    arreglando toyo

--- app/home/index.jsx
+++ app/home/index.jsx
@@ -3,7 +3,6 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const { width, height } = Dimensions.get("window");
 export default function Page() {
 
-  const router = useRouter();  
   return (
     <SafeAreaView className="flex-1 bg-[#1B434D]">
       <StatusBar hidden={false} backgroundColor={"#1B434D"} barStyle={"light-content"} />
@@ -11,26 +10,3 @@ export default function Page() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    alignItems: "center",
-    padding: 24,
-    paddingBottom: 110, // Asegura que el contenido no se superponga con la barra de estado
-
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
